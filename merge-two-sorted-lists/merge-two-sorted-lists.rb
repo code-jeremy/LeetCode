c0742dfb6683e49ea7ea83e85c57e39510860c1c
Jeremy Walters <63620391+code-jeremy@users.noreply.github.com>
2021-10-27 11:14:51 -0400

    Time: 72 ms (49.75%), Space: 210.2 MB (37.88%) - LeetHub

---
+++ merge-two-sorted-lists/merge-two-sorted-lists.rb
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
+# @param {ListNode} l1
+# @param {ListNode} l2
+# @return {ListNode}
+def merge_two_lists(l1, l2)
+    return l2 if l1.nil?
+    return l1 if l2.nil?
+    if l1.val < l2.val
+        l1.next = merge_two_lists(l1.next, l2)
+        return l1
+    else
+        l2.next = merge_two_lists(l1, l2.next)
+        return l2
+    end
+end
